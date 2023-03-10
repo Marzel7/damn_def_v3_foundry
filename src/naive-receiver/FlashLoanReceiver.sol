72a5100065406f1623a1674f1a17faf676531c99
kkeaveney <kevinkeaveney@gmail.com>
2023-03-10 11:33:31 +0000

    implement ERC3156FlashBorrower.onFlashLoan

--- src/naive-receiver/FlashLoanReceiver.sol
+++ src/naive-receiver/FlashLoanReceiver.sol
@@ -1,58 +1,58 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-// import "solady/src/utils/SafeTransferLib.sol";
-// import "openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
-// import "./NaiveReceiverLenderPool.sol";
+import "solady/src/utils/SafeTransferLib.sol";
+import "openzeppelin/interfaces/IERC3156FlashBorrower.sol";
+import "./NaiveReceiverLenderPool.sol";
 
-// /**
-//  * @title FlashLoanReceiver
-//  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
-//  */
-// contract FlashLoanReceiver is IERC3156FlashBorrower {
-//     address private pool;
-//     address private constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+/**
+ * @title FlashLoanReceiver
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract FlashLoanReceiver is IERC3156FlashBorrower {
+    address private pool;
+    address private constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-//     error UnsupportedCurrency();
+    error UnsupportedCurrency();
 
-//     constructor(address _pool) {
-//         pool = _pool;
-//     }
+    constructor(address _pool) {
+        pool = _pool;
+    }
 
-//     function onFlashLoan(
-//         address,
-//         address token,
-//         uint256 amount,
-//         uint256 fee,
-//         bytes calldata
-//     ) external returns (bytes32) {
-//         assembly {
-//             // gas savings
-//             if iszero(eq(sload(pool.slot), caller())) {
-//                 mstore(0x00, 0x48f5c3ed)
-//                 revert(0x1c, 0x04)
-//             }
-//         }
+    function onFlashLoan(
+        address,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata
+    ) external returns (bytes32) {
+        assembly {
+            // gas savings
+            if iszero(eq(sload(pool.slot), caller())) {
+                mstore(0x00, 0x48f5c3ed)
+                revert(0x1c, 0x04)
+            }
+        }
 
-//         if (token != ETH) revert UnsupportedCurrency();
+        if (token != ETH) revert UnsupportedCurrency();
 
-//         uint256 amountToBeRepaid;
-//         unchecked {
-//             amountToBeRepaid = amount + fee;
-//         }
+        uint256 amountToBeRepaid;
+        unchecked {
+            amountToBeRepaid = amount + fee;
+        }
 
-//         _executeActionDuringFlashLoan();
+        _executeActionDuringFlashLoan();
 
-//         // Return funds to pool
-//         SafeTransferLib.safeTransferETH(pool, amountToBeRepaid);
+        // Return funds to pool
+        SafeTransferLib.safeTransferETH(pool, amountToBeRepaid);
 
-//         return keccak256("ERC3156FlashBorrower.onFlashLoan");
-//     }
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
 
-//     // Internal function where the funds received would be used
-//     function _executeActionDuringFlashLoan() internal {}
+    // Internal function where the funds received would be used
+    function _executeActionDuringFlashLoan() internal {}
 
-//     // Allow deposits of ETH
-//     receive() external payable {}
-// }
+    // Allow deposits of ETH
+    receive() external payable {}
+}
